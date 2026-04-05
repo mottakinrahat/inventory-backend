f912282af8a96caad04842a08510ffef9ec8200d
mottakinrahat <rahatchowdhury661@gmail.com>
2026-04-05 13:35:10 +0600

    envAdded

--- dist/config/index.js
+++ dist/config/index.js
@@ -10,15 +10,15 @@ exports.default = {
     env: process.env.NODE_ENV,
     port: process.env.PORT,
     jwt: {
-        jwt_secret: process.env.JWT_SECRET,
-        expires_in: process.env.EXPIRES_IN,
-        refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
-        refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
+        jwt_secret: process.env.JWT_SECRET || process.env.jwt_secret,
+        expires_in: process.env.EXPIRES_IN || process.env.expires_in,
+        refresh_token_secret: process.env.REFRESH_TOKEN_SECRET || process.env.refresh_token_secret,
+        refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN || process.env.refresh_token_expires_in,
     },
     reset_pass: {
-        token_secret: process.env.RESET_PASS_TOKEN,
-        token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
-        link: process.env.RESET_PASS_LINK,
+        token_secret: process.env.RESET_PASS_TOKEN || process.env.reset_pass_token || process.env.reset_pass_token_secret,
+        token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN || process.env.reset_pass_token_expires_in,
+        link: process.env.RESET_PASS_LINK || process.env.reset_pass_link,
     },
-    frontend_url: process.env.FRONTEND_URL,
+    frontend_url: process.env.FRONTEND_URL || process.env.frontend_url,
 };
